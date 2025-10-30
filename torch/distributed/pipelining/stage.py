640605e5a48b2977d8d97f2527f0737537901005
IvanKobzarev <ivan.kobzarev@gmail.com>
2025-10-30 10:45:50 -0700

    [pp] Add reduce_grad Action
    ghstack-source-id: e38a6386af91e08f353096610deddfcd61e387c6
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/166449

--- torch/distributed/pipelining/stage.py
+++ torch/distributed/pipelining/stage.py
@@ -978,7 +978,7 @@ class _PipelineStageBase(ABC):
 
         return ops
 
-    def _post_backward(self, grad_scale_factor: int):
+    def perform_reduce_grad(self, grad_scale_factor: int):
         # Manually call post backward for FSDP
         if isinstance(self.submod, FSDPModule):
             fsdp_module = self.submod
@@ -1001,7 +1001,8 @@ class _PipelineStageBase(ABC):
             distributed_state._root_post_backward_final_callback()
         # Call gradient scaling at the end of the backward pass
         # NOTE: this must happen after FSDP post_backward is FSDP is enabled
-        self.scale_grads(grad_scale_factor)
+        if grad_scale_factor != 1:
+            self.scale_grads(grad_scale_factor)
 
 
 class _PipelineStage(_PipelineStageBase):
