c306435a5c79028d0a767c031356eb54aa2c4aa0
William Wen <williamwen@meta.com>
2025-10-31 03:51:00 +0000

    [dynamo] clean up BaseUserFunctionVariable and LocalGeneratorObjectVariable
    ghstack-source-id: 95b0523b047eced0ac92392c92b31b62fe42adfd
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/166673

--- torch/_dynamo/variables/functions.py
+++ torch/_dynamo/variables/functions.py
@@ -333,6 +333,9 @@ class BaseUserFunctionVariable(VariableTracker):
     def get_name(self):
         return self.get_code().co_name
 
+    def get_globals(self):
+        raise NotImplementedError
+
     def call_function(
         self,
         tx: "InstructionTranslator",
@@ -352,9 +355,6 @@ class BaseUserFunctionVariable(VariableTracker):
             if name == "__name__" and isinstance(self, NestedUserFunctionVariable):
                 result = True
         return variables.ConstantVariable.create(result)
-
-    def inspect_parameter_names(self):
-        return list(inspect.signature(self.get_function()).parameters)
 
     def closure_vars(self, tx):
         return {}
@@ -1149,9 +1149,6 @@ class UserMethodVariable(UserFunctionVariable):
             fn = getattr(self.obj.value, self.fn.__name__)
             return invoke_and_store_as_constant(tx, fn, self.get_name(), args, kwargs)
         return super().call_function(tx, args, kwargs)
-
-    def inspect_parameter_names(self):
-        return super().inspect_parameter_names()[1:]
 
     def var_getattr(self, tx: "InstructionTranslator", name: str):
         if name == "__self__":
