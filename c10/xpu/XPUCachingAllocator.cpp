ec2add2da4cf595a868bb28300e0c2b5f6d183b5
Yu, Guangye <guangye.yu@intel.com>
2025-10-31 20:49:13 +0000

    Introduce a new API torch.accelerator.get_mem_info
    ghstack-source-id: 139d9092d44b0a293f2048fe9df0ee34c1e0ed09
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/156812

--- c10/xpu/XPUCachingAllocator.cpp
+++ c10/xpu/XPUCachingAllocator.cpp
@@ -735,6 +735,10 @@ class XPUAllocator : public DeviceAllocator {
     device_allocators[device]->resetAccumulatedStats();
   }
 
+  std::pair<size_t, size_t> getMemoryInfo(DeviceIndex device) override {
+    TORCH_CHECK_NOT_IMPLEMENTED(false, "Not implemented yet.");
+  }
+
   double getMemoryFraction(DeviceIndex device) {
     assertValidDevice(device);
     return device_allocators[device]->getMemoryFraction();
